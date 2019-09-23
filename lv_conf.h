b24ac35c60f221e3c4d896f0962c410944586996
mwu-ambiq <mwu@ambiqmicro.com>
2019-09-23 16:33:37 +0800

    updated lv_gauge.c

--- lv_conf.h
+++ lv_conf.h
@@ -145,7 +145,7 @@ typedef void * lv_group_user_data_t;
 #define LV_USE_GPU              1
 
 /* 1: Enable file system (might be required for images */
-#define LV_USE_FILESYSTEM       1
+#define LV_USE_FILESYSTEM       0
 #if LV_USE_FILESYSTEM
 /*Declare the type of the user data of file system drivers (can be e.g. `void *`, `int`, `struct`)*/
 typedef void * lv_fs_drv_user_data_t;
